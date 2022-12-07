807e9d5e293f0aa3d6587ff245b4631438009c1c
MNE4 <monashovnikolaye@gmail.com>
2022-12-07 14:25:30 +0300

    RGB ready

--- Frames.py
+++ Frames.py
@@ -37,20 +37,20 @@ class MainFrame(wx.Frame):
         vbox = wx.BoxSizer(wx.VERTICAL)
         hbox = wx.BoxSizer(wx.HORIZONTAL)
 
+        self.cameraSelection = wx.ComboBox(panel, pos=(
+            50, 30), choices=self.camName, style=wx.CB_READONLY)
+        self.cameraSelection.SetSelection(0)
+        hbox.Add(self.cameraSelection, proportion=1,
+                 flag=wx.UP | wx.RIGHT | wx.LEFT, border=2)
+
         self.grayBox = wx.CheckBox(panel, label="Gray")
-        hbox.Add(self.grayBox, proportion=1, flag=wx.LEFT | wx.UP, border=2)
+        hbox.Add(self.grayBox, proportion=1, flag=wx.UP, border=2)
         self.redBox = wx.CheckBox(panel, label="Red")
         hbox.Add(self.redBox, proportion=1, flag=wx.UP, border=2)
         self.greenBox = wx.CheckBox(panel, label="Green")
         hbox.Add(self.greenBox, proportion=1, flag=wx.UP, border=2)
         self.blueBox = wx.CheckBox(panel, label="Blue")
         hbox.Add(self.blueBox, proportion=1, flag=wx.UP, border=2)
-
-        self.cameraSelection = wx.ComboBox(panel, pos=(
-            50, 30), choices=self.camName, style=wx.CB_READONLY)
-        self.cameraSelection.SetSelection(0)
-        hbox.Add(self.cameraSelection, proportion=1,
-                 flag=wx.UP, border=2)
 
         self.acceptBtn = wx.Button(panel, label="Принять")
         hbox.Add(self.acceptBtn, proportion=1, flag=wx.UP | wx.RIGHT, border=2)
@@ -174,16 +174,22 @@ class VideoFrame(wx.Panel):
                 if self.videoMode == "default":
                     vframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
                 elif self.videoMode == "gray":
-                    # vframe = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-                    vframe = cv2.cvtColor(frame, cv2.IMREAD_GRAYSCALE)
-                    vframe = cv2.threshold(vframe, 127, 255, 0)
-                    # vframe = cv2.GaussianBlur(vframe, (9, 9), 0)
+                    vframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 elif self.videoMode == "red":
-                    vframe = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
+                    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    lower_red = np.array([-10, 50, 50])
+                    upper_red = np.array([10, 255, 255])
+                    mask = cv2.inRange(hsv, lower_red, upper_red)
+                    vframe = cv2.bitwise_and(frame, frame, mask=mask)
+                    vframe = cv2.cvtColor(vframe, cv2.COLOR_BGR2RGB)
                 elif self.videoMode == "green":
-                    vframe = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+                    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    lower_green = np.array([50, 50, 50])
+                    upper_green = np.array([70, 255, 255])
+                    mask = cv2.inRange(hsv, lower_green, upper_green)
+                    vframe = cv2.bitwise_and(frame, frame, mask=mask)
+                    vframe = cv2.cvtColor(vframe, cv2.COLOR_BGR2RGB)
                 elif self.videoMode == "blue":
-                    # vframe = cv2.cvtColor(frame, cv2.COLOR_BGR2LUV)
                     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
                     # define range of blue color in HSV
                     lower_blue = np.array([110, 50, 50])
@@ -192,11 +198,12 @@ class VideoFrame(wx.Panel):
                     mask = cv2.inRange(hsv, lower_blue, upper_blue)
                     # Bitwise-AND mask and original image
                     vframe = cv2.bitwise_and(frame, frame, mask=mask)
+                    vframe = cv2.cvtColor(vframe, cv2.COLOR_BGR2RGB)
                 print(vframe.shape)
                 vframe = cv2.putText(vframe, self.camNum, (self.Width - 100, self.Height - 70),
                                      cv2.FONT_HERSHEY_SIMPLEX, 1, (10, 10, 10), 2, cv2.LINE_AA)
-                vframe = cv2.flip(vframe, 1)
-            self.bmp = wx.BitmapFromBuffer(self.width, self.height, vframe)
+                # vframe = cv2.flip(vframe, 1)
+                self.bmp = wx.BitmapFromBuffer(self.width, self.height, vframe)
             # self.bmp.CopyFromBuffer(vframe)
             self.Refresh()
 
